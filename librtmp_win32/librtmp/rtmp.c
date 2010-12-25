3c6eb41f58bcfc564dfd80b79923d20641674896
spriebe <spriebe@899b54ba-5fe6-968d-1f7a-ef89d7bd45fc>
2010-12-25 23:08:10 +0000

    - update zlib to 1.2.4 - fixed rtmp debug build
    git-svn-id: https://disaster123httpfilter.googlecode.com/svn/MP/Async2_http@97 899b54ba-5fe6-968d-1f7a-ef89d7bd45fc

--- librtmp_win32/librtmp/rtmp.c
+++ librtmp_win32/librtmp/rtmp.c
@@ -1247,10 +1247,12 @@ RTMP_ClientPacket(RTMP *r, RTMPPacket *packet)
   return bHasMediaPacket;
 }
 
+/*
 #ifdef _DEBUG
 extern FILE *netstackdump;
 extern FILE *netstackdump_read;
 #endif
+*/
 
 static int
 ReadN(RTMP *r, char *buffer, int n)
@@ -1320,7 +1322,7 @@ ReadN(RTMP *r, char *buffer, int n)
 	}
       /*RTMP_Log(RTMP_LOGDEBUG, "%s: %d bytes\n", __FUNCTION__, nBytes); */
 #ifdef _DEBUG
-      fwrite(ptr, 1, nBytes, netstackdump_read);
+//      fwrite(ptr, 1, nBytes, netstackdump_read);
 #endif
 
       if (nBytes == 0)
@@ -3535,11 +3537,11 @@ int
 RTMPSockBuf_Send(RTMPSockBuf *sb, const char *buf, int len)
 {
   int rc;
-
+/*
 #ifdef _DEBUG
   fwrite(buf, 1, len, netstackdump);
 #endif
-
+*/
 #if defined(CRYPTO) && !defined(NO_SSL)
   if (sb->sb_ssl)
     {
